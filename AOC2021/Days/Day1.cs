cb8ff475917fc1274a6d435d82597d93ed221a1b
robert okoroafor <robert@icentric.co.uk>
2021-12-02 20:11:06 +0000

    Add project files.

---
+++ AOC2021/Days/Day1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2021.Days
+{
+    internal static class Day1
+    {
+        public static void Quesstion1()
+        {
+            var allLines = File.ReadAllLines("inputs/day1.txt").Select(x => Convert.ToInt32(x)).ToArray();
+            var largerMeasurements = allLines.Where((x, idx) => idx > 0 && x > allLines[idx - 1]);
+            Console.WriteLine($"Day 1 question 1 answer is {largerMeasurements.Count()}");
+        }
+    }
+}
---
+++ AOC2021/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AOC2021.Days;
+
+Day1.Quesstion1();
